c55b8f187fa24a7fb8f74c722038ced59da64e75
robatrain <38065788+robatrain@users.noreply.github.com>
2020-12-20 12:56:25 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Message Changed!'));
+app.get('/', (req, res) => res.send('Message Changed Again - Testing Manual Approval'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
